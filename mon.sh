762b59028ced876d5ea9613bda7ff68da3f1638c
Eric Smalling <eric.smalling@snyk.io>
2021-02-24 11:24:15 -0600

    typo fix

--- mon.sh
+++ mon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kubectl create ns snyk-monitor
-kubectl create secret generic snyk-monitor -n snyk-monitor --from-literal=dockercfg.json={} --from-literal=integrationId=$SNYK_ORG_ID
+#kubectl create ns snyk-monitor
+kubectl create secret generic snyk-monitor -n snyk-monitor --from-literal=dockercfg.json={} --from-literal=integrationId=$TF_VAR_snyk_orgid
 
