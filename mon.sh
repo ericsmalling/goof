c841aa3b4532bd63b300a8738dc81e4304ef9b63
Eric Smalling <eric.smalling@snyk.io>
2021-02-24 11:51:35 -0600

    mon.sh install steps added

--- mon.sh
+++ mon.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-#kubectl create ns snyk-monitor
+helm repo add snyk-charts https://snyk.github.io/kubernetes-monitor/
+
+kubectl create ns snyk-monitor
+
 kubectl create secret generic snyk-monitor -n snyk-monitor --from-literal=dockercfg.json={} --from-literal=integrationId=$TF_VAR_snyk_orgid
 
+helm upgrade --install snyk-monitor snyk-charts/snyk-monitor --namespace snyk-monitor --set clusterName="EKS monitored by Snyk"
